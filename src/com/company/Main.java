96f64911504b4858ad4fcc844bf64a45717adcb1
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-09-23 12:54:17 +0200

    Main Klasse geschrieben und kleinigkeiten in NumberTester gemacht

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        NumberTester numberTester = new NumberTester("test.txt");
+
+        numberTester.setOddEvenTester((n) -> (n % 2) == 0);
+        numberTester.setPrimeTester((n) -> {
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        });
+        numberTester.setPalindromeTester((n) -> {
+            String reverse = new StringBuffer(n + "").reverse().toString();
+            return (n + "").equals(reverse);
+
+        });
     }
 }
--- src/com/company/NumberTester.java
+++ src/com/company/NumberTester.java
@@ -2,18 +2,24 @@ package com.company;
 
 public class NumberTester {
 
+    private String filename;
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+
     public NumberTester ( String fileName ) {
-
+        this.filename = fileName;
     }
-    public void setOddEvenTester (NumberTest oddTester ) {
 
+    public void setOddEvenTester (NumberTest oddTester ) {
+        this.oddTester = oddTester;
     }
 
     public void setPrimeTester ( NumberTest primeTester ) {
-
+        this.primeTester = primeTester ;
     }
     public void setPalindromeTester ( NumberTest palindromeTester ) {
-
+        this.palindromeTester = palindromeTester ;
     }
     public void testFile ( ) {
 
