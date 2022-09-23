7c5dc546b8d1d2b9bdb254842e76830014196a74
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-09-23 12:25:43 +0200

    Interface "NumberTest" erstellt und Methoden in "NumberTester" implementiert

---
+++ src/com/company/NumberTest.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface NumberTest {
+    boolean testNumber(int number);
+}
--- src/com/company/NumberTester.java
+++ src/com/company/NumberTester.java
@@ -2,5 +2,22 @@ package com.company;
 
 public class NumberTester {
 
+    public NumberTester ( String fileName ) {
+
+    }
+    public void setOddEvenTester (NumberTest oddTester ) {
+
+    }
+
+    public void setPrimeTester ( NumberTest primeTester ) {
+
+    }
+    public void setPalindromeTester ( NumberTest palindromeTester ) {
+
+    }
+    public void testFile ( ) {
+
+    }
+
 
 }
