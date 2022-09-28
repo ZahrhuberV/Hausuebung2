b80bc908a687ace4d18d5b75fe2518c2a545d4f9
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-09-28 11:30:05 +0200

    "VectorCalculator" Klasse erstellt

---
+++ src/com/company/VectorCalculator.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class VectorCalculator extends AbstractCalculator{
+
+    public VectorCalculator(CalculationOperation add,
+                            CalculationOperation substract,
+                            CalculationOperation multiply,
+                            CalculationOperation divide) {
+        super(add, substract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number substract(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return null;
+    }
+}
