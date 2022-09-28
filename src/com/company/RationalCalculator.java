0f0f0b7987a34cabe6d434d5fd7fbfbbb0e29e41
ZahrhuberV <valentin.zahrhuber@gmx.at>
2022-09-28 11:28:49 +0200

    "RationalCalculator" Klasse erstellt

---
+++ src/com/company/RationalCalculator.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class RationalCalculator extends AbstractCalculator{
+
+    public RationalCalculator(CalculationOperation add,
+                              CalculationOperation substract,
+                              CalculationOperation multiply,
+                              CalculationOperation divide) {
+        super(add, substract, multiply, divide);
+    }
+
+    @Override
+    public Number add(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number substract(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number multiply(Number a, Number b) {
+        return null;
+    }
+
+    @Override
+    public Number divide(Number a, Number b) {
+        return null;
+    }
+}
